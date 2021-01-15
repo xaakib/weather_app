232fb36d62de3a00a8babcc8c9f71d01a8dbd6e5
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-15 22:27:15 +0600

    some done

--- lib/widgets/single_weather.dart
+++ lib/widgets/single_weather.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:weather_app/models/wather_locations.dart';
 class SingleWeather extends StatelessWidget {
   final int index;
   SingleWeather(this.index);
